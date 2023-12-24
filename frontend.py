e670854669d67de0dd0a2ee5631b2654f4bc8bcf
Darkside099 <tarunmawri2005@gmail.com>
2023-12-24 21:44:30 +0530

    update

--- frontend.py
+++ frontend.py
@@ -1,39 +1,37 @@
 import tkinter as tk
 from tkinter import messagebox
+from Backend import Server
 
 
+def exit_program():
+    if messagebox.askokcancel("Exit", "Are you sure you want to exit?"):
+        win.destroy()
 
 
+win = tk.Tk()
+win.geometry('1000x500')
+win.title('SIMS')
+win.iconbitmap(r'D:\VS_CODE\Git\Porject_2-Sales_And_Inventory_Managment_System\icon.ico')
 
-def main_win():
-    win = tk.Tk()
-    win.geometry('1000x500')
-    win.title('SIMS')
-    win.iconbitmap(r'D:\VS_CODE\Git\Porject_2-Sales_And_Inventory_Managment_System\icon.ico')
-    
-    def exit_program():
-        if messagebox.askokcancel("Exit", "Are you sure you want to exit?"):
-            win.destroy()
+btn_add = tk.Button(win, text="Add Item")#command=show_add)
+btn_add.pack()
 
-    btn_add = tk.Button(win, text="Add Item")#command=show_add)
-    btn_add.pack()
+btn_see = tk.Button(win, text="See Items")#command=show_see)
+btn_see.pack()
 
-    btn_see = tk.Button(win, text="See Items")#command=show_see)
-    btn_see.pack()
+btn_update = tk.Button(win, text="Update Item") #command=show_update)
+btn_update.pack()
 
-    btn_update = tk.Button(win, text="Update Item") #command=show_update)
-    btn_update.pack()
+btn_delete = tk.Button(win, text="Delete Item")#command=show_delete)
+btn_delete.pack()
 
-    btn_delete = tk.Button(win, text="Delete Item")#command=show_delete)
-    btn_delete.pack()
+btn_customer_billing = tk.Button(win, text="Customer Billing")#command=show_customer_billing)
+btn_customer_billing.pack()
 
-    btn_customer_billing = tk.Button(win, text="Customer Billing")#command=show_customer_billing)
-    btn_customer_billing.pack()
+btn_billing_details = tk.Button(win, text="Billing Details")#command=show_billing_details)
+btn_billing_details.pack()
 
-    btn_billing_details = tk.Button(win, text="Billing Details")#command=show_billing_details)
-    btn_billing_details.pack()
+btn_exit = tk.Button(win, text="Exit")#command=exit_program)
+btn_exit.pack()
 
-    btn_exit = tk.Button(win, text="Exit")#command=exit_program)
-    btn_exit.pack()
-
-    win.mainloop()
+win.mainloop()
