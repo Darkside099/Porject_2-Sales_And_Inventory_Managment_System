f8422d1155b44a14b5ded69cb5c3bd898ce89765
Darkside099 <tarunmawri2005@gmail.com>
2023-12-24 23:59:54 +0530

    done

--- Main.py
+++ Main.py
@@ -1,23 +1,39 @@
 
-from frontend import Application
 from Backend import Server
 from tkinter import *
 
-root = Tk()
-root.geometry('1000x500')
-root.title('SIMS')
-root.iconbitmap(r'D:\VS_CODE\Git\Porject_2-Sales_And_Inventory_Managment_System\Other_Resources\icon.ico')
-
 def login():
-    x,y=uname_entry.get(),password_entry.get()
+    root = Tk()
+    root.geometry('1000x500')
+    root.title('SIMS')
+    root.iconbitmap(r'D:\VS_CODE\Git\Porject_2-Sales_And_Inventory_Managment_System\Other_Resources\icon.ico')
+    def check():
+        username,password=uname_entry.get(),password_entry.get()
+        if Server.connection(username,password) == True:
+            login_status.config(text='Login Successfuly',fg='blue')
+            root.destroy()
+        else:
+            login_status.config(text='Try Again',fg='red')
 
-uname_lable = Label(root,text='Username :')
-uname_lable.pack()
-uname_entry = Entry(root,width=30)
-uname_entry.pack()
-password_lable = Label(root,text='Password :')
-password_lable.pack()
-password_entry = Entry(root,show='*',width = 30)
-password_entry.pack()
+    login_label = Label(root, text='WELCOME TO SALES AND INVENTORY MANAGEMENT SYSTEM\nSERVER LOGIN', font=("Cascadia code bold",15))
+    login_label.pack()
 
-button = Button(root,text='Login',command=login)
+
+    uname_lable = Label(root,text='Username :',font=("Cascadia code",10))
+    uname_lable.pack()
+    uname_entry = Entry(root,width=30)
+    uname_entry.pack()
+    password_lable = Label(root,text='Password :',font=("Cascadia code",10))
+    password_lable.pack()
+    password_entry = Entry(root,show='*',width = 30)
+    password_entry.pack()
+
+    button = Button(root,text='Login',command=check)
+    button.pack()
+
+    login_status = Label(root, text="")
+    login_status.pack()
+
+    root.mainloop()
+
+Server.connection('root','2005')
